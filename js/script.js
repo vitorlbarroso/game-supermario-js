e5c606ed0d45d57904456c120d8ccaf7eb07acef
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-05-31 14:39:14 -0300

    ADD adicionadas as telas de gamer over e pressione para iniciar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+var isFirstRound = true;
+
 const mario = document.querySelector('.game-mario-running');
 const pipe = document.querySelector('.game-pipe');
+const cloudsDiv = document.querySelector('.clouds');
 const clouds = document.querySelector('#clouds');
+const montainsDiv = document.querySelector('.montains');
 const montains = document.querySelector('#montains');
+const pressToStart = document.querySelector('.press-start-game');
+const gameOver = document.querySelector('.press-reaload-game-over');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -16,28 +22,53 @@ const verifyKey = () => {
     }
 }
 
-const loop = setInterval(() => {
-    const heightMario = +window.getComputedStyle(mario).bottom.replace('px','');
-    const pipePosition = pipe.offsetLeft;
-    const cloudsPosition = clouds.offsetLeft;
-    const montainsPosition = montains.offsetLeft;
-    if (pipePosition <= 120 && pipePosition > 0 && heightMario < 100) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
-        
-        mario.style.animation = 'none';
-        mario.style.bottom = `${heightMario}px`;
-        mario.src = 'img/mario_dead.png';
-        mario.style.width = '70px';
-        
-        clouds.style.animation = 'none';
-        clouds.style.left = `${cloudsPosition}px`;
-        
-        montains.style.animation = 'none';
-        montains.style.left = `${montainsPosition}px`;
-        
-        clearInterval(loop);
+const startFirstGame = () => {
+    const reloadPageGameOver = () => {
+        gameOver.style.display = 'flex';
+        const verifyKeyReloadGameOver = () => { event.keyCode == 38 ? document.location.reload(false) : document.location.reload(false) }
+        document.addEventListener('keydown', verifyKeyReloadGameOver);
     }
-}, 10)
+    
+    const game = () => {
+        const loop = setInterval(() => {
+            const heightMario = +window.getComputedStyle(mario).bottom.replace('px','');
+            const pipePosition = pipe.offsetLeft;
+            const cloudsPosition = clouds.offsetLeft;
+            const montainsPosition = montains.offsetLeft;
+            if (pipePosition <= 120 && pipePosition > 0 && heightMario < 100) {
+                
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
+                
+                mario.style.animation = 'none';
+                mario.style.bottom = `${heightMario}px`;
+                mario.src = 'img/mario_dead.png';
+                mario.style.width = '70px';
+                
+                clouds.style.animation = 'none';
+                clouds.style.left = `${cloudsPosition}px`;
+                
+                montains.style.animation = 'none';
+                montains.style.left = `${montainsPosition}px`;
+                
+                clearInterval(loop);
+                reloadPageGameOver();
+            } else {
+                document.addEventListener('keydown', verifyKey)
+            }
+        }, 10);
+    }
+    
+    if (event.keyCode == 38) {
+        mario.classList.remove('pause-initial-mario');
+        pipe.classList.remove('pause-initial-pipe');
+        cloudsDiv.classList.remove('pause-initial-clouds');
+        montainsDiv.classList.remove('pause-initial-montains');
+        pressToStart.style.display = 'none';
+        game();
+    }
+}
 
-document.addEventListener('keydown', verifyKey);
+if (isFirstRound == true) {
+    document.addEventListener('keydown', startFirstGame);
+}
