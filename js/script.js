a4d113c977eafbda60899229f8029688149aaee0
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-05-31 15:58:57 -0300

    ADD adicionado o sistema de contagem de pontos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ const montains = document.querySelector('#montains');
 const pressToStart = document.querySelector('.press-start-game');
 const gameOver = document.querySelector('.press-reaload-game-over');
 
+let recordCoins = sessionStorage.getItem('recordCoins') > 0 ? sessionStorage.getItem('recordCoins') : 0;
+let quantitysCoins = 0;
+
+document.querySelector('#recordCoins').innerHTML = recordCoins;
+
 const jump = () => {
     mario.classList.add('jump');
     setTimeout(() => {
@@ -35,8 +40,11 @@ const startFirstGame = () => {
             const pipePosition = pipe.offsetLeft;
             const cloudsPosition = clouds.offsetLeft;
             const montainsPosition = montains.offsetLeft;
-            if (pipePosition <= 120 && pipePosition > 0 && heightMario < 100) {
-                
+            if (pipePosition <= 140 && heightMario > 105) {
+                quantitysCoins += 1;
+                document.querySelector('#quantitysCoins').innerHTML = quantitysCoins;
+            }
+            if ((pipePosition <= 130) && (pipePosition > 0) && (heightMario < 100)) {
                 pipe.style.animation = 'none';
                 pipe.style.left = `${pipePosition}px`;
                 
@@ -52,11 +60,13 @@ const startFirstGame = () => {
                 montains.style.left = `${montainsPosition}px`;
                 
                 clearInterval(loop);
+                quantitysCoins > recordCoins ? sessionStorage.setItem('recordCoins', quantitysCoins) : false;
+                console.log(recordCoins)
                 reloadPageGameOver();
             } else {
                 document.addEventListener('keydown', verifyKey)
             }
-        }, 10);
+        }, 100);
     }
     
     if (event.keyCode == 38) {
